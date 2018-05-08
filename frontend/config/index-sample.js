177438560d7084fc192db6e2344508372e16952b
Paul Barnes <Paul.Barnes@moi-global.com>
2018-05-08 12:49:24 +0100

    remove config from repo, use sample instead

---
+++ frontend/config/index-sample.js
@@ -0,0 +1,2 @@
+// Set the URL where your GraphQL queries should go
+export const GRAPHQL_ENDPOINT = 'http://vue-wp.localhost/graphql'
--- frontend/config/index.js
+++
@@ -1,2 +0,0 @@
-// Set the URL where your GraphQL queries should go
-export const GRAPHQL_ENDPOINT = 'http://vue-cms-starter.localhost/graphql'
